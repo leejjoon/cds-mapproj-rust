4443fb763f844ec18c2d7a164fc598beb61ddf0e
Jae-Joon Lee <lee.j.joon@gmail.com>
2025-06-06 16:34:26 +0900

    Fix SIP distortion application to use world coordinates

--- src/img2proj.rs
+++ src/img2proj.rs
@@ -315,8 +315,11 @@ impl ImgXY2ProjXY for WcsWithSipImgXY2ProjXY {
     let y_undistorted = self.wcs.cd21 * x_pix + self.wcs.cd22 * y_pix;
     
     // 3. Apply SIP distortion in world coordinates
-    let x_distorted = x_undistorted + self.sip.f(x_pix, y_pix);
-    let y_distorted = y_undistorted + self.sip.g(x_pix, y_pix);
+    // The SIP standard (Greisen et al. 2002, A&A, 395, 1077) states that the
+    // SIP polynomials f(u,v) and g(u,v) are functions of intermediate world
+    // coordinates (u,v), which are x_undistorted, y_undistorted in our case.
+    let x_distorted = x_undistorted + self.sip.f(x_undistorted, y_undistorted);
+    let y_distorted = y_undistorted + self.sip.g(x_undistorted, y_undistorted);
     
     ProjXY::new(x_distorted, y_distorted)
   }
